265ab42a75be6ed44de64abc98ac34a80bbe995d
zverygo <simogin.roman@gmail.com>
2016-06-29 15:09:38 +0300

    делаю форму для ввода переменных

--- www/view/page/matrix.php
+++ www/view/page/matrix.php
@@ -1,1 +1,29 @@
-HI
+<?php
+$x = (int)($_POST['x']);
+$n = (int)($_POST['n']);
+echo 'Количество переменных = '.$x.'<br>';
+echo 'Количество неравенств = '.$n.'<br>';
+?>
+
+
+<?php ?>
+
+    <form method="post" action="../../index.php?action=matrix">
+        <?php
+            echo '<table>';
+                for ($i = 0; $i < $x; $i++ ){
+                    echo '<tr>';
+                    for ($j = 0; $j < $n; $j++){
+                        echo '<td>'.'<input name='."$i.$j".' autofocus required>'.'</td>';
+                    }
+                    echo '</tr>';
+                }
+            echo '</table>';
+        ?>
+        <input type="submit" value="Save" class="btn">
+    </form>
+
+<?php ?>
+
+
+
