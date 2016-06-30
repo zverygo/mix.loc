dcaeeeddd166145f510bdc1f28fd768b4f213fc6
zverygo <simogin.roman@gmail.com>
2016-06-30 10:29:45 +0300

    форма готова

--- www/view/page/matrix.php
+++ www/view/page/matrix.php
@@ -9,11 +9,32 @@ echo 'Количество неравенств = '.$n.'<br>';
 <form method="post" action="index.php?x=<?=$x?>&n=<?=$n?>&action=can">
     <table>
         <?php
+            echo '<tr>';
+            for ($r = 0; $r < $x; $r++){
+                echo '<td><input name="'.'r'.$r.'"</td>';
+            }        
+            echo '<td>
+                    <select name="m-m">
+                        <option value="max">max</option>
+                        <option value="min">min</option>
+                    </select>
+                </td>';
+            echo '</tr>';
             for ($i =0; $i < $n; $i++){
                 echo '<tr>';
                 for ($j = 0; $j < $x; $j++){
-                    echo '<td><input name="'.$i.'-'.$j.'"</td>';
+                    echo '<td><input name="'.'a'.$i.'-'.$j.'"</td>';
                 }
+                echo '<td>
+                        <select name="'.$i.'ner">
+                            <option value="m">m</option>
+                            <option value="m-r">m-r</option>
+                            <option value="r">r</option>
+                            <option value="b-r">b-r</option>
+                            <option value="b">b</option>
+                        </select>
+                      </td>';
+                echo '<td><input name="'.'b'.$i.'"</td>';
                 echo '</tr>';
             }
         ?>
@@ -28,12 +49,20 @@ echo 'Количество неравенств = '.$n.'<br>';
     <form method="post" action="../../index.php?action=can">
         <?php
             echo '<table>';
-                for ($i = 0; $i < $x; $i++ ){
+                echo '<tr>';
+                    for ($r = 0; $r < $x; $r++){
+                        echo '<td>'.$_POST['r'.$r].'</td>';
+                    }        
+                echo '<td>'.$_POST['m-m'].'</td>';
+                echo '</tr>';
+                for ($i = 0; $i < $n; $i++ ){
                     echo '<tr>';
-                    for ($j = 0; $j < $n; $j++){
+                    for ($j = 0; $j < $x; $j++){
                         
-                        echo '<td>'.$_POST[$i.'-'.$j].'</td>';
+                        echo '<td>'.$_POST['a'.$i.'-'.$j].'</td>';
                     }
+                    echo '<td>'.$_POST[$i.'ner'].'</td>';
+                    echo '<td>'.$_POST['b'.$i].'</td>';
                     echo '</tr>';
                 }
             echo '</table>';
--- www/view/test/test.php
+++ www/view/test/test.php
@@ -1,29 +1,7 @@
-<?php
-$x = (int)($_POST['x']);
-$n = (int)($_POST['n']);
-echo 'Количество переменных = '.$x.'<br>';
-echo 'Количество неравенств = '.$n.'<br>';
-?>
-
-
-    <form method="post" action="view/page/can.php">
-        <?php
-            echo '<table>';
-                for ($i = 0; $i < $n; $i++ ){
-                    echo '<tr>';
-                    for ($j = 0; $j < $x; $j++){
-                        echo '<td>'.'<input name="'.$i.'.'.$j.'" placeholder="'.$i.'-'.$j.'" autofocus required>'.'</td>';
-                    }
-                    echo '</tr>';
-                }
-            echo '</table>';
-        ?>
-        <div class="form-group">
-        <label>test</label>
-        <br>
-        <input name="test" required>
-        </div>
-        <input type="submit" value="Save" class="btn">
-    </form>
-
-
+<select name="num">
+    <option value="-2"><</option>
+    <option value="-1"><=</option>
+    <option value="0">=</option>
+    <option value="1">>=</option>
+    <option value="2">></option>
+</select>
