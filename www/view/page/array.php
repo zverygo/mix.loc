7b7128c42fca72bd4b697d46cd55e0c3d516ed29
zverygo <simogin.roman@gmail.com>
2016-07-15 14:21:10 +0300

    добавил массив с оценками

--- www/view/page/array.php
+++ www/view/page/array.php
@@ -1,36 +1,51 @@
 <?php
 
-$arr = array (
-                array (1,2,3),
-                array (4,0,6)
+//массив определяющий целевую функцию
+$arr1 = array (5,10);
+//переменная определяющая куда стремиться ЦФ, по умолчанию к max
+$m = 'max';
+//$m = 'min';
 
-);
+//массив определяющий векторы А  (неравенства)
+$arr2 = array (
+                array (4,5,1),
+                array (3,8,4)
+              );
+//масив определяющий опорное решение
+$arr3 = array (96,144,48);
 
+echo $ii=count($arr1).'<br>';
+echo $nn=count($arr2).'<br>';
+echo $jj=count($arr2[0]).'<br>';
+
+//массив с номерами векторов образующих базис
+echo 'массив с номерами векторов образующих базис';
+for ($i=0;$i<$;$i++){
+    $ar = array_map(function ($el1, $el2) {
+        return $el1 * $el2;
+    },
+    $arr4, $arr2[$i]);
+    $ai[$i] = array_sum($ar) - $arr1[$i];
+
+//массив 
+$arr4 = array (0,0,0);
+
+//массив оценок
+echo 'массив оценок';
+for ($i=0;$i<$ii;$i++){
+    $ar = array_map(function ($el1, $el2) {
+        return $el1 * $el2;
+    },
+    $arr4, $arr2[$i]);
+    $ai[$i] = array_sum($ar) - $arr1[$i];
+}
 echo '<pre>';
-print_r ($arr);
+print_r ($ai);
 echo '</pre><br><br>';
 
-echo $arr[1][1];
-echo '<br><br>';
+
+
 
 
 ?>
-
-<table>
-    <?php
-        for ($i = 0; $i < 3; $i++) {
-            echo '<tr>';
-            for ($j = 0; $j < 3; $j++){
-                if (isset($arr[$i][$j])) {
-                    $arr[$i][$j] = $arr[$i][$j] - $arr[$i][0]*$arr[$i][1]/$arr[1][0];
-                    echo '<td>'.$arr[$i][$j].'</td>';
-                }
-                
-            }
-            echo '</tr>';
-        }
-        
-    
-    ?>
-</table>
 
