8dda1f08ff114b3041b48ca97670c23fcf90424c
zverygo <simogin.roman@gmail.com>
2016-07-22 16:25:26 +0300

    добавил обработку массива arr3

--- www/view/test/test.php
+++ www/view/test/test.php
@@ -3,43 +3,32 @@
 $x = 3;
 
 $n = 3;
-
+//матрица записана по векторам
 $arr2 = array (
                 array (1,2,4),
                 array (-2,2,-1),
                 array (1,-1,1)
               );
-
-
 $arr3 = array (0,3,5);
 
-/*echo '<pre>';
-print_r($arr2);
-//print_r($arr3);
-echo '</pre>';*/
-
-/*$a = $arr2[0][1]/$arr2[0][0];
-
-$arr2[0][1] = $arr2[0][1]-$arr2[0][0]*$a;
-$arr2[1][1] = $arr2[1][1]-$arr2[1][0]*$a;
-$arr2[2][1] = $arr2[2][1]-$arr2[2][0]*$a;
-
-$a = $arr2[0][2]/$arr2[0][0];
-
-$arr2[0][2] = $arr2[0][2]-$arr2[0][0]*$a;
-$arr2[1][2] = $arr2[1][2]-$arr2[1][0]*$a;
-$arr2[2][2] = $arr2[2][2]-$arr2[2][0]*$a;
-
-$a = $arr2[1][2]/$arr2[1][1];
-
-$arr2[1][2] = $arr2[1][2]-$arr2[1][1]*$a;
-$arr2[2][2] = $arr2[2][2]-$arr2[2][1]*$a;
-
+for ($i=0;$i<$n-1;$i++) {
+    for ($j=$i;$j<$n-1;$j++) {
+        $a=$arr2[$i][$j+1]/$arr2[$i][$i];
+        for($k=$i;$k<$n;$k++) {
+            $arr2[$k][$j+1]-=$arr2[$k][$i]*$a;
+        }
+        $arr3[$j+1]-=$arr3[$i]*$a;
+    }
+}
+echo '<br>матрица записана по векторам';
 echo '<pre>';
 print_r($arr2);
-echo '</pre>';*/
+print_r($arr3);
+echo '</pre>';
 
-$arr2_2 = array (
+
+// матрица записанна по неравенствам
+/*$arr2_2 = array (
                 array (1,-2,1),
                 array (2,2,-1),
                 array (4,-1,1)
@@ -48,9 +37,6 @@ $arr2_2 = array (
 
     
 for ($i=0;$i<$n-1;$i++) {
-    //echo '<br>__'.$arr2_2[$j+1][$i];
-    //echo '<br>__'.$arr2_2[$j][$i];
-    //echo '<br>'.$a=$arr2_2[$j+1][$i]/$arr2_2[$j][$i];
     for ($j=$i;$j<$n-1;$j++) {
         echo '<br>__'.$b=$arr2_2[$j+1][$i];
         echo '<br>__'.$c=$arr2_2[$i][$i];
@@ -58,15 +44,10 @@ for ($i=0;$i<$n-1;$i++) {
         for($k=$i;$k<$n;$k++) {
             $arr2_2[$j+1][$k]=$arr2_2[$j+1][$k]-$arr2_2[$i][$k]*$a;
         }
-        echo '<pre>';
-        print_r($arr2_2);
-        echo '</pre>';
     }
-    /*echo '<pre>';
-    print_r($arr2_2);
-    echo '</pre>';*/
 }
-/*echo '<pre>';
+echo '<br>матрица записанна по неравенствам';
+echo '<pre>';
 print_r($arr2_2);
 echo '</pre>';*/
 
