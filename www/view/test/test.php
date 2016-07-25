bd089d06681311002db8181212bc06df44b3f03c
zverygo <simogin.roman@gmail.com>
2016-07-25 11:19:30 +0300

    тихий ужас

--- www/view/test/test.php
+++ www/view/test/test.php
@@ -1,39 +1,9 @@
-<?
-
-
-// -------НЕ ТРОГАТЬ!!!----------
-/*$x = 4;
-
-$n = 4;
-//матрица записана по векторам
-$arr2 = array (
-                array (3,1,-2,1),
-                array (2,-1,-2,5),
-                array (1,4,-3,-1),
-                array (1,-1,1,4)
-              );
-$arr3 = array (-2,-1,9,4);
-
-for ($i=0;$i<$n-1;$i++) {
-    for ($j=$i;$j<$n-1;$j++) {
-        $a=$arr2[$i][$j+1]/$arr2[$i][$i];
-        for($k=$i;$k<$n;$k++) {
-            $arr2[$k][$j+1]-=$arr2[$k][$i]*$a;
-        }
-        $arr3[$j+1]-=$arr3[$i]*$a;
-    }
-}
-echo '<br>матрица записана по векторам';
-echo '<pre>';
-print_r($arr2);
-print_r($arr3);
-echo '</pre>';*/
-////////
+<?php
 
 $x = 4;
 
 $n = 4;
-//матрица записана по векторам
+//матрица записана по неравенствам
 $arr2 = array (
                 array (3,1,-2,1),
                 array (2,-1,-2,5),
@@ -42,32 +12,60 @@ $arr2 = array (
               );
 $arr3 = array (-2,-1,9,4);
 
+echo '<pre>';
+print_r($arr2);
+//print_r($arr3);
+echo '</pre>';
+
+//прямой ход метода Гаусса
 for ($i=0;$i<$n-1;$i++) {
     for ($j=$i;$j<$n-1;$j++) {
-        $a=$arr2[$i][$j+1]/$arr2[$i][$i];
+        $a=$arr2[$j+1][$i]/$arr2[$i][$i];
         for($k=$i;$k<$n;$k++) {
-            $arr2[$k][$j+1]-=$arr2[$k][$i]*$a;
+            $arr2[$j+1][$k]-=$arr2[$i][$k]*$a;
         }
         $arr3[$j+1]-=$arr3[$i]*$a;
     }
 }
+echo '<br> прямой ход';
+echo '<pre>';
+print_r($arr2);
+//print_r($arr3);
+echo '</pre>';
 
-for ($j=0;$j<$n;$j++) {
+//обратный ход
+/*for ($i=$n-1;$i>=0;$i--) {
+    for ($j=$i;$j>0;$j--) {
+        //echo '<br>__'.$arr2[$j-1][$i];
+        //echo '<br>__'.$arr2[$i][$i];
+        //echo '<br>'.$a=$arr2[$j-1][$i]/$arr2[$i][$i];
+        $a=$arr2[$j-1][$i]/$arr2[$i][$i];
+        for($k=$i;$k>=0;$k--) {
+            //echo '<br>    '.$arr2[$j-1][$k];
+            $arr2[$j-1][$k]-=$arr2[$i][$k]*$a;    
+            //$arr2[$k][$j-1]-=$arr2[$k][$i]*$a;
+            
+        }
+        //echo '<pre>';
+        //print_r($arr2);
+        //echo '</pre>';
+        //$arr3[$j-1]-=$arr3[$i]*$a;
+    }
+}
+
+echo '<br> обратный ход';
+echo '<pre>';
+print_r($arr2);
+//print_r($arr3);
+echo '</pre>';*/
+
+
+/*for ($j=0;$j<$n;$j++) {
     $arr2i[$j][0]=$arr2[$j][0]/$arr2[0][0];
 }
 $arr3[0]/=$arr2[0][0];
 for ($j=0;$j<$n;$j++) {
     $arr2[$j][0]=$arr2i[$j][0];
-}
-
-
-echo '<br>матрица записана по векторам';
-echo '<pre>';
-print_r($arr2);
-print_r($arr3);
-echo '</pre>';
-
-
-
+}*/
 
 ?>
